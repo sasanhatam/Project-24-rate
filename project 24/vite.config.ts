dff552d452acfaf03b99e3312a8f4fd529efd2ad
Brokerir <thecompany.sasan@gmail.com>
2026-01-30 02:41:48 +0330

    Add files via upload

---
+++ project 24/vite.config.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        widget: path.resolve(__dirname, 'widget.html'),
+      },
+    },
+  },
+});
